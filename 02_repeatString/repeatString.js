8574ce088836ac342e892f9f6b5918acc8cfffb0
BigSunys <126293740+BigSunys@users.noreply.github.com>
2023-09-03 15:29:05 +0200

    fin exo 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let resultat = '';
+    if (num < 0) {
+        return "ERROR"
+    }else{
+        for (let index = 0; index < num; index++) {
+            
+            resultat += string
+            
+        }
+        return resultat 
+    }
+    
 };
-
+console.log(repeatString('hey',5))
 // Do not edit below this line
 module.exports = repeatString;

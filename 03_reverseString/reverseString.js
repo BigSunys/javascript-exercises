c4f6609345c2f2c3596f0f5b806712ba35b623b8
BigSunys <126293740+BigSunys@users.noreply.github.com>
2023-09-03 15:40:16 +0200

    fin exo 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let newString = "";
+for (let index = string.length - 1; index >= 0; index--) {
+    newString += string[index]
+}
+return newString
 };
 
+// console.log(reverseString('hello'))
 // Do not edit below this line
 module.exports = reverseString;
